fb78470337eabba7e70c8b11d57c3ec32aaf3a7d
sunshineOfficial <44614650+sunshineOfficial@users.noreply.github.com>
2021-04-19 18:13:36 +0300

    Ban, kick and some fun stuff

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ import discord
 import os
 import re
 from urllib import parse, request
+import requests
+import pymorphy2
+import random
+from datetime import datetime as dt
+import pytz
 
 
-TOKEN = 'ODI4MTk0OTk3MjE1MDM1NDE2.YGmCsg.6XUA7VnLL3Q8nH4mzBoA7rKJ074'
+TOKEN = 'ODI4MTk0OTk3MjE1MDM1NDE2.YGmCsg.XULFRLsfnrYENx2iQb-WLQA-KR8'
 
 ydl_opts = {
     'format': 'bestaudio/best',
@@ -18,10 +23,11 @@ ydl_opts = {
 }
 
 playlist = {}
+dashes = ['\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685']
 
 
 def endSong(guild, path, voice_client):
-    if len(playlist[guild.id]) > 1 and path != playlist[guild.id][1]:
+    if len(playlist[guild.id]) > 0 or len(playlist[guild.id]) > 1 and path != playlist[guild.id][1]:
         os.remove(path)
     playlist[guild.id] = playlist[guild.id][1:]
     if playlist[guild.id]:
@@ -75,6 +81,9 @@ class MusicBot(commands.Cog):
     async def leave(self, ctx):
         if len(self.bot.voice_clients) > 0:
             await self.bot.voice_clients[0].disconnect()
+            for i in range(len(playlist[ctx.message.guild.id]) - 1, -1, -1):
+                os.remove(playlist[ctx.message.guild.id][i])
+            playlist[ctx.message.guild.id].clear()
         else:
             await ctx.send('Я не в войс-чате')
 
@@ -107,6 +116,120 @@ class MusicBot(commands.Cog):
             endSong(guild, playlist[guild.id][0], self.bot.voice_clients[0])
         else:
             await ctx.send('Я не в войс-чате')
+
+    @commands.command(name='cat')
+    async def cat(self, ctx):
+        url = 'https://api.thecatapi.com/v1/images/search'
+        response = requests.get(url)
+        await ctx.send(response.json()[0]['url'])
+
+    @commands.command(name='dog')
+    async def dog(self, ctx):
+        url = 'https://dog.ceo/api/breeds/image/random'
+        response = requests.get(url)
+        await ctx.send(response.json()['message'])
+
+    @commands.command(name='numerals')
+    async def numerals(self, ctx, word, number):
+        morph = pymorphy2.MorphAnalyzer()
+        p = morph.parse(word)[0]
+        await ctx.send(f'{number} {p.make_agree_with_number(int(number)).word}')
+
+    @commands.command(name='alive')
+    async def alive(self, ctx, word):
+        morph = pymorphy2.MorphAnalyzer()
+        p = morph.parse(word)[0]
+        await ctx.send(f'{word.capitalize()} {"живой" if p.tag.animacy is not None else "не живой"}')
+
+    @commands.command(name='noun')
+    async def noun(self, ctx, word, case, state):
+        morph = pymorphy2.MorphAnalyzer()
+        p = morph.parse(word)[0]
+        await ctx.send(p.inflect({state[:4], case}).word)
+
+    @commands.command(name='inf')
+    async def inf(self, ctx, word):
+        morph = pymorphy2.MorphAnalyzer()
+        p = morph.parse(word)[0]
+        await ctx.send(p.normal_form)
+
+    @commands.command(name='morph')
+    async def morph(self, ctx, word):
+        morph = pymorphy2.MorphAnalyzer()
+        p = morph.parse(word)[0]
+        await ctx.send(f"{p.tag.POS} {p.tag.animacy} {p.tag.gender} {p.tag.case} {p.tag.number}")
+
+    @commands.command(name='roll_dice')
+    async def roll_dice(self, ctx, count):
+        res = [random.choice(dashes) for _ in range(int(count))]
+        await ctx.send(" ".join(res))
+
+    @commands.command(name='ban')
+    async def ban(self, ctx, name, *reason):
+        if not ctx.message.mentions:
+            await ctx.send("Отметь участника через @")
+            return
+        if not ctx.message.author.guild_permissions.ban_members:
+            await ctx.send("У вас нет прав на использование данной команды")
+        else:
+            if not reason:
+                reason = ['Причина', 'не', 'указана']
+
+            ban_embed = discord.Embed(
+                title=f'{ctx.message.author.name} забанил {ctx.message.mentions[0].name}',
+                description=f'Причина: {" ".join(reason)}',
+                color=discord.Colour.random(),
+                timestamp=dt.now(pytz.timezone('GMT'))
+            )
+
+            await ctx.message.mentions[0].ban()
+            await ctx.send(embed=ban_embed)
+
+    @commands.command(name='kick')
+    async def kick(self, ctx, name, *reason):
+        if not ctx.message.mentions:
+            await ctx.send("Отметь участника через @")
+            return
+        if not ctx.message.author.guild_permissions.kick_members:
+            await ctx.send("У вас нет прав на использование данной команды")
+        else:
+            if not reason:
+                reason = ['Причина', 'не', 'указана']
+
+            kick_embed = discord.Embed(
+                title=f'{ctx.message.author.name} кикнул {ctx.message.mentions[0].name}',
+                description=f'Причина: {" ".join(reason)}',
+                color=discord.Colour.random(),
+                timestamp=dt.now(pytz.timezone('GMT'))
+            )
+
+            await ctx.message.mentions[0].kick()
+            await ctx.send(embed=kick_embed)
+
+    @commands.command(name='heads_or_tails')
+    async def heads_or_tails(self, ctx):
+        await ctx.send('Монета подбрасывается...')
+        x = random.randint(0, 100)
+        if x < 50:
+            await ctx.send(':full_moon: Орёл!')
+        elif x > 50:
+            await ctx.send(':new_moon: Решка!')
+        else:
+            await ctx.send(':last_quarter_moon: Монета упала ребром!')
+
+    @commands.command(name='clear')
+    async def clear(self, ctx, n):
+        if not ctx.message.author.guild_permissions.manage_messages:
+            await ctx.send("У вас нет прав на использование данной команды")
+        else:
+            try:
+                n = int(n)
+            except ValueError:
+                await ctx.send('Неверные данные')
+                return
+
+            await ctx.channel.delete_messages(await ctx.channel.history(limit=n).flatten())
+            await ctx.send(f'Удалено {n} сообщений')
 
 
 bot = commands.Bot(command_prefix='$')
